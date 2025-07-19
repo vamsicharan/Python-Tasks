7e5e9d0bc823e0b5f4fc008036517f4b0590d951
vamsi_charan <108205550+vamsicharan@users.noreply.github.com>
2025-07-19 23:06:04 +0530

    Add files via upload

---
+++ str/conversion.py
@@ -0,0 +1,9 @@
+
+name="jOHNwICK"
+result=""
+for i in name:
+    if i.isupper():
+        result +=i.lower() # change to upper
+    elif (i.islower()):   
+        result +=i.upper()   # change to lower
+print(result) 
