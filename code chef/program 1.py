b181cfda752ee65a75d365c09c98777e9c9b2b1b
vamsi_charan <108205550+vamsicharan@users.noreply.github.com>
2025-08-16 22:22:07 +0530

    Add files via upload

---
+++ code chef/program 1.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().split()))
+    c=0
+    for j in l:
+        k=l.count(j)
+        if k>=3:
+            c+=1
+    if c>1:
+        print("NO")
+    else:
+        print("yes")
